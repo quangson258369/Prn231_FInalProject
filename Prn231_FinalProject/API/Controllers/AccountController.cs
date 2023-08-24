57c90a629e54b51e221d85c2230b7d14ec83d11a
datquangtran <45448911+datquangtran@users.noreply.github.com>
2023-08-25 01:45:18 +0700

    update api

---
+++ Prn231_FinalProject/API/Controllers/AccountController.cs
@@ -0,0 +1,165 @@
+﻿using API.Extensions;
+using API.Models;
+using API.Models.Request;
+using API.Models.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly QuanLyKhoContext _dbcontext;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(SignInManager<User> signInManager,
+            UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager,
+            QuanLyKhoContext dbcontext,
+            IConfiguration configuration)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _dbcontext = dbcontext;
+        }
+
+        [HttpPost("GetToken")]
+        [AllowAnonymous]
+        public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest login)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
+
+                if (!result.Succeeded)
+                {
+                    return BadRequest("Login failed");
+                }
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, login.Email)
+                    };
+
+                claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var expiry = DateTime.Now.AddDays(Convert.ToInt32(_configuration["Jwt:ExpiryInDays"]));
+
+                var token = new JwtSecurityToken(
+                    _configuration["Jwt:Issuer"],
+                    _configuration["Jwt:Audience"],
+                    claims,
+                    expires: expiry,
+                    signingCredentials: creds
+                );
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return new LoginResponse
+                {
+                    Token = tokenString
+                };
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = await _userManager.FindByEmailAsync(request.Email);
+                if (user != null)
+                {
+                    ModelState.AddModelError("Email", "Email is already taken.");
+                    return Conflict(ModelState);
+                }
+
+                var newUser = new User
+                {
+                    UserName = request.Email,
+                    Password = request.Password,
+                };
+
+                var createResult = await _userManager.CreateAsync(newUser, request.Password);
+                if (!createResult.Succeeded)
+                {
+                    foreach (var error in createResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+
+                var addToRoleResult = await _userManager.AddToRoleAsync(newUser, "user");
+                if (!addToRoleResult.Succeeded)
+                {
+                    foreach (var error in addToRoleResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+
+                return Created(string.Empty, null);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpDelete("Logout")]
+        [Authorize]
+        public IActionResult Logout()
+        {
+            try
+            {
+                // Remove the token cookie from the client
+                Response.Cookies.Delete("AccessToken");
+                // Invalidate the user's token by adding it to a blacklist
+                CustomMiddleware.BlackListTokens.Add(ApiContext.Current.Token, DateTime.Now);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions gracefully and return an appropriate response
+                return StatusCode(500, $"Logout failed: {ex.Message}");
+            }
+        }
+
+    }
+}
--- Prn231_FinalProject/API/Controllers/SupplierController.cs
+++ Prn231_FinalProject/API/Controllers/SupplierController.cs
@@ -7,8 +7,8 @@ namespace API.Controllers
     [ApiController]
     public class SupplierController : ControllerBase
     {
-        private QuanlyKhoContext _context;
-        public SupplierController(QuanlyKhoContext context)
+        private QuanLyKhoContext _context;
+        public SupplierController(QuanLyKhoContext context)
         {
             _context = context;
         }
@@ -17,5 +17,74 @@ namespace API.Controllers
         {
             return Ok(_context.Supliers.ToList());
         }
+        [HttpGet("GetAllSupplier")]
+        public IActionResult GetAllSupplier()
+        {
+            try
+            {
+                var sup = _context.Supliers
+                    .ToList();
+
+                return Ok(sup);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while retrieving Input records.");
+            }
+        }
+
+        [HttpPost("CreateSupplier")]
+        public IActionResult CreateSupplier(Suplier sup)
+        {
+            try
+            {
+                sup.Id = 0;
+                _context.Supliers.Add(sup);
+                _context.SaveChanges();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Có lỗi xảy ra trong quá trình tạo khách hàng.");
+            }
+        }
+        [HttpGet("{id}")]
+        public ActionResult<List<Customer>> GetSupplierById(string id)
+        {
+            var sup = _context.Objects
+                .Where(input => input.Id == id)
+                .ToList();
+            if (sup == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sup);
+        }
+
+        [HttpDelete("DeleteSupplier/{supplierId}")]
+        public IActionResult DeleteSupplier(int supplierId)
+        {
+            try
+            {
+                // Tìm khách hàng cần xóa trong cơ sở dữ liệu
+                var supplierToDelete = _context.Supliers.Find(supplierId);
+
+                if (supplierToDelete == null)
+                {
+                    return NotFound("Supplier không tồn tại."); // Trả về 404 nếu không tìm thấy khách hàng
+                }
+                _context.Supliers.Remove(supplierToDelete);
+                _context.SaveChanges();
+
+                return NoContent(); // Trả về 204 để thể hiện xóa thành công
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Có lỗi xảy ra trong quá trình xóa Supplier.");
+            }
+        }
+
     }
 }
