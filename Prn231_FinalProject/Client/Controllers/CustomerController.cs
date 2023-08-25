2041306ff638399a31b93ea47fcc6c624cc95871
datquangtran <45448911+datquangtran@users.noreply.github.com>
2023-08-25 09:38:49 +0700

    update client

--- Prn231_FinalProject/Client/Controllers/CustomerController.cs
+++ Prn231_FinalProject/Client/Controllers/CustomerController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace Client.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient client = null;
+        private string GetAllCustomersApiUrl = "";
+        private string CreateCustomerApiUrl = "";
+        private string GetCustomerByIdApiUrl = "";
+        private string DeleteCustomerApiUrl = "";
+
+        public CustomerController() {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            GetAllCustomersApiUrl = "http://localhost:7073/api/GetAllCustomers";
+            CreateCustomerApiUrl = "http://localhost:7073/api/CreateCustomer";
+            GetCustomerByIdApiUrl = "http://localhost:7073/api/GetCustomerById";
+            DeleteCustomerApiUrl = "http://localhost:7073/api/DeleteCustomer/";
+
+        }
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Customer> customers = new List<Customer>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:7073/api/GetAllCustomers"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        customers = JsonConvert.DeserializeObject<List<Customer>>(apiResponse);
+                    }
+                    else
+                    {
+                        // Handle the error response
+                    }
+                }
+            }
+
+            return View(customers);
         }
     }
+
 }
--- Prn231_FinalProject/Client/Controllers/OutputController.cs
+++ Prn231_FinalProject/Client/Controllers/OutputController.cs
@@ -1,6 +1,5 @@
 ﻿using Client.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
@@ -14,87 +13,20 @@ namespace Client.Controllers
         public static List<Output> _output;
         public static List<Models.Object> _products;
         public static List<Models.Customer> _customers;
-        private string ProductApiUrl = "";
-        private string InputApiUrl = "";
-        private string OutputApiUrl = "";
-        private string CustomerAPIUrl = "";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
         public OutputController()
         {
             client = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             client.DefaultRequestHeaders.Accept.Add(contentType);
-            ProductApiUrl = "https://localhost:7073/api/Product";
-            InputApiUrl = "https://localhost:7073/api/Input";
-            OutputApiUrl = "https://localhost:7073/api/Output";
-            CustomerAPIUrl = "https://localhost:7073/api/Customer";
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            HttpResponseMessage response = await client.GetAsync(ProductApiUrl);
-            string strData = await response.Content.ReadAsStringAsync();
-            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-            List<Models.Object> listProducts = JsonSerializer.Deserialize<List<Models.Object>>(strData, option);
-            var selectListProduct = new List<SelectListItem>();
-            foreach (var item in listProducts)
-            {
-                selectListProduct.Add(new SelectListItem { Text = "" + item.DisplayName, Value = "" + item.Id });
-            }
-            ViewBag.SelectListProduct = selectListProduct;
-
-            HttpResponseMessage response2 = await client.GetAsync(CustomerAPIUrl);
-            string strData2 = await response2.Content.ReadAsStringAsync();
-            List<Models.Customer> listCustomers = JsonSerializer.Deserialize<List<Models.Customer>>(strData2, option);
-            var selectListCustomer = new List<SelectListItem>();
-            foreach (var item in listCustomers)
-            {
-                selectListCustomer.Add(new SelectListItem { Text = "" + item.DisplayName, Value = "" + item.Id });
-            }
-            ViewBag.SelectListCustomer = selectListCustomer;
-
-            return View();
+            OutApiUrl = "http://localhost:5292/api/AuthorsControllerAPI";
         }
         [HttpPost]
-        public async Task<IActionResult> addOutPut(OutputInfo output)
-        {
-            DateTime timeOutput = DateTime.Now;
-            var id = GenerateRandom();
-            HttpResponseMessage response = await client.GetAsync(OutputApiUrl);
-            string strData = await response.Content.ReadAsStringAsync();
-            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-            List<Models.Output> listOutput = JsonSerializer.Deserialize<List<Models.Output>>(strData, option);
-            foreach (var item in listOutput)
-            {
-                if (item.Id.Equals("" + id))
-                {
-                    id = GenerateRandom();
-                }
-            }
-            Output o = new Output()
-            {
-                Id = "" + id,
-                DateOutput = timeOutput,
-            };
-
-            string data = JsonSerializer.Serialize(output);
-            await client.PostAsync(OutApiUrl + "", new StringContent(data, Encoding.UTF8, "application/json"));
-
-
-
-
-            output.IdOutput = "" + id;
-
-            if (ModelState.IsValid)
-            {
-                string data2 = JsonSerializer.Serialize(output);
-                await client.PostAsync(OutApiUrl, new StringContent(data, Encoding.UTF8, "application/json"));
-                return RedirectToAction(nameof(Index));
-            }
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> editOutPut(Output output)
+        public async Task<IActionResult> addOutPut(Output output)
         {
             if (ModelState.IsValid)
             {
@@ -103,12 +35,6 @@ namespace Client.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View();
-        }
-        public int GenerateRandom()
-        {
-            Random random = new Random();
-            return random.Next(10000, 100000); // Generates a random number between 10000 and 99999
-
         }
     }
 }
